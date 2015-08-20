13f7d503f3775c44e23b1c92a0cca3123c20570e
kqr <k@rdw.se>
2015-08-20 21:39:41 +0200

    well... not working but meh

--- src/Main.hs
+++ src/Main.hs
@@ -2,46 +2,98 @@
 
 module Main where
 
-import Prelude hiding (take)
+import Prelude hiding (readFile)
 import Control.Applicative (liftA2)
-import Data.Attoparsec.Text (Parser, parseOnly, string, char, endOfInput, takeTill, decimal, (<?>))
-import Data.Text (Text)
+import Data.Attoparsec.Text
+import Data.Either (partitionEithers)
+import Data.Monoid ((<>))
+import Control.Monad (filterM, (<=<))
+import Data.Text (Text, pack)
+import Data.Text.IO (readFile)
 import Data.Time.Calendar (Day, fromGregorianValid)
-import System.Directory (getDirectoryContents)
+import System.Directory (getDirectoryContents, doesFileExist)
 
 
 -- Wrapper for slugs so we don't mix 'em up with regular text
 data Slug = Slug Text deriving Show
 
--- This should ensure the text is actually a sluggish value
-toSlug :: Text -> Either String Slug
-toSlug = return . Slug
+
+data Blog = Blog
+    { drafts :: [Post]
+    , published :: [Post]
+    }
 
 
 data Post = Post
     { title :: Text
     , slug :: Slug
-    , timestamp :: Day
-    , content :: ()        -- probably some sort of Html-y type to begin with
+    , datestamp :: Day
+    , content :: Text
     , author :: ()         -- for a more civilized age
     , tags :: [()]         -- also for a more civilized age
     }
+    deriving Show
 
 
 main = do
     putStrLn "stub"
--- code for a more civilized age:
---    drafts <- getPosts =<< getDirectoryContents "drafts"
---    published <- getPosts =<< getDirectoryContents "published"
--- getPosts :: [FilePath] -> IO [Post]
--- readPost :: FilePath -> IO Post
+
+
+getPosts :: FilePath -> IO [Post]
+getPosts directory = do
+    files <- mapM readPost =<< getFilesIn directory
+    let (errors, posts) = partitionEithers (map (uncurry parsePost) files)
+    mapM error errors
+    return posts
+
+
+getFilesIn :: FilePath -> IO [FilePath]
+getFilesIn directory = do
+    -- bajskorv funkar inte med relativa paths och kataloger och sånt >_<
+    inodes <- getDirectoryContents directory
+    let fullpaths = map (\inode -> directory ++ "/" ++ inode) inodes
+    filterM doesFileExist fullpaths
+
+
+
+readPost :: FilePath -> IO (FilePath, Text)
+readPost filename = do
+    filetext <- readFile filename
+    return (filename, filetext)
+
+
+
+
+annotate :: FilePath -> String -> String
+annotate filename s =
+    "Invalid post '" <> filename <> "': " <> s
+
+
+parsePost :: FilePath -> Text -> Either String Post
+parsePost filename filetext =
+    either (Left . annotate filename) return $ do
+        (datestamp, slug) <- parseFilename (pack filename)
+        (title, content) <- parseContent filetext
+        return Post
+            { title = title
+            , slug = slug
+            , datestamp = datestamp
+            , content = content
+            , author = ()
+            , tags = []
+            }
+
+
+parseContent :: Text -> Either String (Text, Text)
+parseContent =
+    parseOnly (liftA2 (,) (takeTill isEndOfLine <* endOfLine) takeText)
 
 
 -- parses a filename in the form of "2015-08-21-this-is-a-slug.txt"
 -- into a tuple (2015-04-21,Slug "this-is-a-slug")
 parseFilename :: Text -> Either String (Day, Slug)
 parseFilename =
-    parseOnly (liftA2 (,) dateParser slugParser <* string ".txt" <* endOfInput)
+    parseOnly (liftA2 (,) dateParser slugParser)
 
 
 -- parser that consumes something like "2015-08-21" and turns it into a
@@ -57,11 +109,7 @@ dateParser = do
     maybe (fail "invalid date") return (fromGregorianValid year month day)
 
 
--- parser that consumes any text up to a period (should probably be something
--- else going forward...) and turns it into a Slug
 slugParser :: Parser Slug
-slugParser = do
-    slug <- fmap toSlug (takeTill (== '.'))
-    either fail return slug
-
+slugParser =
+    fmap Slug (takeWhile1 (inClass "a-z0-9-") <?> "post must have a slug!")
 
